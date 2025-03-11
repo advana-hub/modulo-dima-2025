48d4f6cf1d1af6201344e1f7c107b965691c0dc9
Samuele Fiorini <Samuele.Fiorini@gruppoiren.it>
2025-03-11 09:08:58 +0100

    feat(classes): L01 part 1 and part 2 ready

---
+++ classes/L01_1_data_science_intro.py
@@ -0,0 +1,264 @@
+
+# /// script
+# dependencies = [
+#   "marimo==0.11.17",
+# ]
+# ///
+
+import marimo
+
+__generated_with = "0.11.17"
+app = marimo.App(
+    width="full",
+)
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    mo.md("""# Benvenuti! 👋""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Chi""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## Quando/dove
+
+        - una lezione a settimana, il mercoledì pomeriggio, nell’orario 14:00-17:00
+        - aula PC3 (al quarto piano, di fronte alla biblioteca)
+        - dal 12 marzo al 28 maggio, con l'esclusione del 23 e 30 aprile (pausa festività)
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## Come
+
+        - sessione di esami: mese di giugno (da concordare)
+        - consigliamo a ciascuno di voi di utilizzare il proprio laptop personale
+        - prerequisiti (nice-to-have, non bloccanti!): 🐍 Programmazione 2 (48382), 🧠 Machine Learning (114944)
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## Cosa
+
+        1. Cos’è la Data Science? + modern Python per Data Scientist
+        2. Preparazione dati + Analisi esplorativa
+        3. Feature engineering
+        4. Dimensionality reduction
+        5. Clustering
+        6. Topic modeling
+        7. Regression
+        8. Qualche lezione bonus 👀
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        """
+        ## Warm-up game
+
+        [Graphs](https://www.graphs.world/)
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""# Cos'è la Data Science?""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        f"""
+        ## Un po' di storia
+
+        - [The History Of Data Science and Pioneers You Should Know](https://onlinestemprograms.wpi.edu/blog/history-data-science-and-pioneers-you-should-know)
+        - [Data Science timeline](https://timeline.historyofdatascience.com/)
+        {mo.image(src="./data_science_timeline.png", caption="https://medium.com/towards-data-science/the-roots-of-data-science-77c71115229")}
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## Qualche momento topico
+
+        - 1943: Warren McCulloch e Walter Pitts pubblicano il paper [“A Logical Calculus of the ideas Imminent in Nervous Activity](https://home.csulb.edu/~cwallis/382/readings/482/mccolloch.logical.calculus.ideas.1943.pdf);
+        - 1949: Nicholas Metropolis e Stanislav Ulam pubblicano il paper [The Monte Carlo Method](https://people.bordeaux.inria.fr/pierre.delmoral/MetropolisUlam49.pdf), basato sul loro lavoro di ricerca svolto nel decennio precedente all'interno del Progetto Manhattan nei laboratori di Los Alamos;
+        - 1957: [Arthur Samuel](https://en.wikipedia.org/wiki/Arthur_Samuel_(computer_scientist)), pioniere dell'AI e contributor di TeX, conia il termine _"machine learning"_;
+        - 1962: [John Tukey](https://en.wikipedia.org/wiki/John_Tukey), ideatore dell'algoritmo FFT, getta le basi teoriche per una definizione della futura Data Science in _"The Future of Data Analysis"_, indicando per primo ciò che la differenzia dalla statistica:
+
+            > _**procedures** for analyzing data, **techniques** for interpreting the results of such procedures, ways of **planning the gathering of data to make its analysis easier**, more precise or more accurate, and **all the machinery and results of (mathematical) statistics** which apply to analyzing data_;
+
+        - 1970: [Edgar Codd](https://en.wikipedia.org/wiki/Edgar_F._Codd) della IBM definisce il concetto di _"database relazionale"_;
+
+        - 1986: [Geoffrey Hinton](https://en.wikipedia.org/wiki/Geoffrey_Hinton) pubblica il paper [Learning representations by back-propagating errors](https://www.cs.utoronto.ca/~hinton/absps/naturebp.pdf), che definisce uno standard per il processo di apprendimento delle reti neurali profonde.
+
+            /// tip | Nobel per la Fisica 2024
+            Per questo e suoi altri lavori, è stato insignito del premio Nobel per la Fisica nel 2024!
+            ///
+
+        - 2007: Prima release della libreria Python che diventerà lo standard per il machine learning: [scikit-learn](https://en.wikipedia.org/wiki/Scikit-learn)!
+        - 2008: Il termine _"data scientist"_ emerge come buzzword online (nel 2012 verrà definito "il lavoro più sexy del secolo!")
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        /// tip
+        Fin dall'inizio era quindi chiaro agli addetti ai lavori che i problemi che avrebbe dovuto affrontare la data science non erano _nuovi di per sè_, ma il fatto che riguardassero il (giovane) mondo dei dati poneva delle complessità nuove relativamente all'approccio.
+        ///
+
+        In tempi più recenti, non tutti gli statistici sono risultati persuasi del fatto che la data science fosse una _nuova_ disciplina:
+
+        > _Aren’t **we** Data Science?_ <small>Marie Davidian, American Statistical Association President, 2013</small>
+
+        > Is data science just a **rebranding** of statistics? <small>Martin Goodson, Royal Statistical Society, 2015</small>
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        [Leo Breiman](https://en.wikipedia.org/wiki/Leo_Breiman), co-ideatore dell'algoritmo Random Forest, ha infine identificato due obiettivi principali della _scienza dei dati_:
+
+        1. **prediction** ➜ to be able to predict what the responses are going to be to future input variables;
+        2. **inference** ➜ infer how nature is associating the response variables to the input variables.
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        /// tip | Recap
+        Possiamo quindi riassumere dicendo: la data science è una disciplina che tenta di applicare il metodo sperimentale e principi/strumenti statistici all'analisi dei dati, occupandosi anche degli aspetti tecnici relativi a procedure e tecniche di acquisizione e gestione dati, al fine ultimo di produrre modelli descrittivi del dato analizzato, approfondire la relazione tra le variabili in input e le variabili in output (target) di un modello e predirre le variabili di output in corrispondenza di input noti.
+        ///
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## Quali strumenti si usano?
+
+        Una definizione (anonima) non poi così distante dalla realtà cita:
+
+        > _A Data Scientist is a person who is better at statistics than any software engineer and better at software engineering than any statistician._
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+            rf"""
+            E' ormai chiaro infatti che spesso un Data Scientist non si limita a padroneggiare matematica, statistica e gli aspetti di modellazione, ma deve anche "sporcarsi le mani" con il mondo del software, dell'ingegneria dei dati, del cloud computing, etc. In questo senso, proviamo a fare una lista (disordinata!) di strumenti e competenze utili ad un Data Scientist:
+    
+            - **conoscenze in svariate branche della matematica**: algebra lineare, algebra modulare, teoria degli insiemi, problemi inversi e teoria della regolarizzazione, ricerca operativa, probabilità, statistica descrittiva, statistica inferenziale, metodi bayesiani, metodi montecarlo, calcolo numerico, analisi di Fourier e teoria dei segnali, ...
+            - **conoscenze di software engineering e database**: SQL/NoSQL, calcolo parallelo e distribuito, DevOps/MLOps, versioning del codice, cloud computing, programmazione in vari linguaggi...
+            - **visualizzazione dati e informazioni**: sia dal punto di vista tecnico (che libreria di quale linguaggio uso?) sia dal punto di vista teorico (è meglio mappare una certa info tramite il colore o la dimensione?)
+            - **machine learning & co.**: supervised vs unsupervised, deep learning, reinforcement learning, generative AI e LLM, ...
+            - **soft skills**: comunicazione scientifica, business translation, teamwork, capacità di problem e framing di un problema sconosciuto...
+            - **project management**: pianificazione e controllo di tempi e costi, rispetto scadenze, ...
+            - **competenze di dominio**: telco, energy, supply chain, sales & marketing, pharma, logistics, ...
+
+            {mo.image(src="data-science-landscape.jpg")}
+            """
+        )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        f"""
+        ## Topology of data professions
+
+        <!-- <iframe src="https://excalidraw.com/#json=9U_RDdeKx2Tg4SQiZpIE3,JtO8qRPcUsmBMD169zzhgg" style="width:100%;height:100px"> -->
+
+        {mo.image(src="topology_data_prof.svg", caption="https://excalidraw.com/#json=9U_RDdeKx2Tg4SQiZpIE3,JtO8qRPcUsmBMD169zzhgg")}
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        rf"""
+        ## Machine Learning
+
+        A livello tecnico, uno degli aspetti caratterizzanti che distingue un Data Scientist da un Data Analyst e da un Data Engineer sono le competenze verticali, spesso generaliste, sul **machine learning** (quelle specialistiche le lasciamo ai Machine Learning Engineers!)
+
+        {mo.image(src="./ml_map.svg")}
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## L'analogia della cucina!""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.image(src="./kitchen_analogy.png", caption="https://excalidraw.com/#json=dRNsELmZ1KWGTHahyrBse,76yV4koB0Q3QTuNorMedlg")
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ classes/L01_2_modern_python_for_ds.py
@@ -0,0 +1,320 @@
+# /// script
+# dependencies = [
+#   "marimo==0.11.17",
+# ]
+# ///
+
+import marimo
+
+__generated_with = "0.11.17"
+app = marimo.App(width="full")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        """
+        # Hello world!
+
+        👋 Questo è un primo esempio di _notebook interattivo_ in cui è possibile eseguire codice Python. La libreria che implementa tale strumento si chiama [marimo](https://github.com/marimo-team/marimo).
+
+        /// note | Info
+        In questa lezione approfondiremo il mondo Python, i concetti di notebook e librerie e tanto altro.
+        ///
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        """
+        # Prima di iniziare: qualche info utile su Python
+
+        ![](https://imgs.xkcd.com/comics/python.png)
+        from [XKCD](https://xkcd.com/353/)
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## Principi di design
+
+        - Python è un linguaggio creato negli anni '90, ma ciònonostante negli anni si è modernizzato ed è tutt'ora lo standard nel mondo analisi dati, data science, machine learning, AI (e non solo!).
+
+        - Supporta sia programmazione _ad oggetti_ (OOP, object-oriented programming), sia programmazione _funzionale_ (FP, functional programming) ed altri paradigmi di programmazione.
+
+        - Al posto di parentesi o punteggiatura, utilizza la spaziatura (indentazione e/o whitespaces) come informazione _semantica_ per delimitare istruzioni e blocchi di codice.
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        Esiste una lista di "principi" di design, detta [The Zen of Python](https://peps.python.org/pep-0020/#the-zen-of-python), che nelle intenzioni del creatore del linguaggio dovrebbero essere seguiti da ogni sviluppatore. Eccone un estratto:
+
+        ```
+        Beautiful is better than ugly.
+        Explicit is better than implicit.
+        Simple is better than complex.
+        Complex is better than complicated.
+        Flat is better than nested.
+        Sparse is better than dense.
+        Readability counts.
+        Special cases aren't special enough to break the rules.
+        Although practicality beats purity.
+        Errors should never pass silently.
+        Unless explicitly silenced.
+        There should be one-- and preferably only one --obvious way to do it.
+        Now is better than never.
+        Although never is often better than *right* now.
+        If the implementation is hard to explain, it's a bad idea.
+        If the implementation is easy to explain, it may be a good idea.
+        ```
+
+        /// tip |
+        Python ha quindi tanti lati in comuni con alcuni principi matematici: un buon codice Python, come una buona dimostrazione, dovrebbe essere **semplice**, **comprensibile** e in qualche modo **esteticamente appagante**.
+        ///
+
+        <!-- ## 1.2 Duck typing
+
+        Python è un linguaggio [dynamically typed](https://en.wikipedia.org/wiki/Dynamic_programming_language): semplificando, i tipi delle variabili vengono valutati a _runtime_ (ovvero durante l'esecuzione di un dato script) e possono mutare, a differenza di quanto accade nei linguaggi _statically typed_ nei quali i tipi vengono verificati durante una fase di _compilazione_ - che nella pratica di Python è "trasparente", non va gestita esplicitamente - e non possono mutare.
+
+        Questo rende Python più conciso e semplice da scrivere, ma anche più prono ad errori e più lento di altri linguaggi (es. TypeScript). Il suo approccio alla tipizzazione è definito [duck typing](https://en.wikipedia.org/wiki/Duck_typing):
+
+        !!! tip "🦆 Duck typing"
+            _If it walks like a duck and it quacks like a duck, then it must be a duck!_ -->
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md("""# Come si può iniziare a utilizzare Python?""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## Installazione
+
+        Esistono davvero tantissimi modi per installare Python, qui consigliamo i nostri preferiti (che sono attualmente _i più comodi_ da utilizzare per la data science):
+
+        - ✨ tramite [uv](https://docs.astral.sh/uv/guides/install-python/#installing-python), una new-entry nell'ecosistema di gestori di librerie e progetti Python
+        - tramite [miniconda](https://docs.anaconda.com/miniconda/)
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## Utilizzo
+
+        Esistono forse ancora più modi per utilizzare Python; di nuovo, consigliamo i seguenti perchè sono degli standard nel mondo della data science:
+
+        - notebook interattivi:
+            - ✨ [marimo](https://docs.marimo.io/getting_started/index.html), una new-entry nell'ecosistema di strumenti di sviluppo Python, ancora molto "giovane" ma promettente
+            - [JupyterLab](https://jupyterlab.readthedocs.io/en/latest/getting_started/installation.html), un ambiente di sviluppo costruito intorno all'utilizzo di [notebook interattivi](https://jupyterlab.readthedocs.io/en/latest/user/notebook.html)
+            - prodotti commerciali (es. Google Colab, Hugging Face, Kaggle)
+        - IDE (_integrated development environment_):
+            - ✨ [Visual Studio Code (VSC)](https://code.visualstudio.com/), distribuito da Microsoft, molto completo che supporta anche i notebook Jupyter
+            - PyCharm
+        - terminali:
+            - una qualunque shell Windows (es. `cmd`, `PowerShell`)
+            - una qualunque shell Linux (es. `bash`)
+            - una qualunque shell macOS (es. `zsh`)
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## Estensione
+
+        Python implementa un set di funzionalità molto basilari dette _standard library_. Per estendere tali funzionalità si deve quasi sempre installare _librerie_ esterne (detti anche _pacchetti_), ognuno deputato a implementare una o più funzionalità complesse dedicate ad uno specifico scopo: visualizzazione dati, gestione di web server, crittografia, calcolo simbolico, etc.
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        Gli ingredienti essenziali per installare nuove librerie sono:
+
+        - un _virtual environment_ -> un ambiente isolato (pensiamo ad un folder su disco) dedicato ad un progetto specifico, in cui è installata una versione di Python (potenzialmente diversa da quella installata al punto (3.1)) e le librerie esterne necessarie al progetto
+        - un tool per _installare e gestire_ le librerie esterne all'interno del virtual environment -> una volta installate esse diventano le **dipendenze** del progetto cui si sta lavorando (e si parla quindi di _dependencies management_)
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        E' quindi consigliabile avere (almeno) un virtual environment dedicato a ciascun progetto cui si sta lavorando, per evitare di avere conflitti di gestione delle varie versioni delle librerie e ritrovarsi in quello che è definito [dependency hell](https://www.xkcd.com/1987/) 👿
+
+        ![](https://imgs.xkcd.com/comics/python_environment.png)
+
+        Esistono tanti tool alternativi per creare virtual environment (tra cui i già citati uv e miniconda), mentre il principale tool per installazione di pacchetti si chiama [pip](https://pip.pypa.io/en/stable/) (_package installer for Python_).
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## Runbook
+
+        Riassumendo, per partire con Python consigliamo di seguire i passi:
+
+        1. aprire un terminale
+        2. installare [uv](https://docs.astral.sh/uv/getting-started/installation/)
+        3. posizionarsi nel folder desiderato (tramite `cd nome_folder`) e creare un virtual environment tramite [uv](https://docs.astral.sh/uv/pip/environments/#creating-a-virtual-environment): `uv venv --python 3.12`
+        4. installare le librerie necessarie tramite `uv pip install` (uv avrà cura di installarle nell'environment appena creato!)
+        5. scegliere uno strumento di sviluppo (IDE e/o notebook), aprire un nuovo notebook/file Python e importare le librerie tramite `import nome_libreria` oppure `import nome_libreria as alias_libreria`
+        6. sviluppare e divertirsi!
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        rf"""
+        # Come si possono utilizzare i notebook di questo corso?
+
+        Il materiale del corso verrà rilasciato sulla piattaforma GitHub, in questa Git repository: https://github.com/advana-hub/modulo-dima-2025
+
+        Potete procedere:
+
+        - eseguendo direttamente ciascun notebook grazie all'installazione di uv, tramite il comando `uvx marimo run https://github.com/advana-hub/modulo-dima-2025/blob/main/classes/<NOME_LEZIONE>.py`
+        - oppure per una strada più classica, che richiede però qualche setup aggiuntivo: {mo.accordion({"Espandi": """
+        1. installare [git](https://git-scm.com/downloads)
+        2. clonare la repository tramite `git clone https://github.com/advana-hub/modulo-dima-2025.git`
+        3. eseguire/modificare ogni notebook localmente tramite `uvx marimo edit <NOME_LEZIONE>.py` (se avete creato un venv e installato marimo, non serve nemmeno il comando `uvx`)
+        """})}
+
+
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        f"""
+        # Python essentials
+
+        type hierarchy (**semplificata!**), per chi vuole solo la cruda verità [qui](https://docs.python.org/3/reference/datamodel.html#the-standard-type-hierarchy) c'è la gerarchia completa
+
+        {mo.image(src="./type_hierarchy.svg", caption="https://excalidraw.com/#json=nbhV07l_tp8bjcbIknT1f,zjuvujbH_PCPtLdyDmaHjg")}
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.carousel([mo.md(
+        r"""
+        esistono svariati _tipi_ basilari, tra cui i principali sono:
+
+        - `int` -> numero intero
+        - `float` -> numero decimale
+        - `str` -> stringa di testo
+        - `bool` -> booleano
+        """), 
+
+                 mo.md(
+        r"""ci sono poi _tipi_ dedicati a strutture dati più complesse:
+
+            - `list` -> collezione di oggetti referenziati rispetto alla propria _posizione_ (quindi, ordinabile)
+                <!-- - `tuple` -> come la lista, ma _immutabile_ -->
+            <!-- - `set` -> equivalente del concetto matematico di insieme (non ordinabile, senza ripetizioni, ...) -->
+            - `dict` -> collezione di oggetti referenziati rispetto alla _chiave_ cui sono associati, ovvero mappa tra un insieme di _chiavi_ ed un insieme di _valori_
+            <!-- - questi tipi supportano la definizione (efficiente!) tramite _comprehension_ -->
+            - `DataFrame` -> struttura ormai standard per implementare il concetto di tabella (implementate da librerie esterne, es. della library per elaborazione dati `pandas`)"""),
+
+                 mo.md(
+        r"""
+    - a seconda del proprio tipo, ogni variabile espone alcuni metodi (funzioni) con cui può essere manipolata, accessibili tramite la notazione `variabile.metodo()`
+    - esistono gli operatori standard aritmetici (+, -, *, /, etc.) e di confronto (>, <, >=, !=, ==, `in`, `is`, `not`, etc.)
+    - esiste la possibilità di commentare il codice tramite `#`
+    - esistono i classici strumenti per il _controllo del flusso_: clausole `if/elif/else`, cicli `for` e `while`, gestione degli errori tramite `try/except`, `match/case`
+    - esiste la possibilità di definire funzioni tramite `def`, le quali potranno accettare input sia in maniera _posizionale_ (arguments) che _nominale_ (keyword arguments), ricalcando la differenza tra `list` e `dict`
+    - esiste la possibilità di definire anche nuovi "oggetti" (classi) tramite `class` che, di fatto, sono nuovi _tipi_ 🤯
+    - tante funzionalità di MATLAB sono disponibili nella libreria [numpy](https://numpy.org/doc/stable/user/numpy-for-matlab-users.html), che sta per _numerical Python_, ed è alla base di tantissimi strumenti Python ormai standard per l'algebra lineare, il calcolo scientifico, l'analisi dati e il machine learning -> Numpy è il backend di pandas, ma ormai è quasi del tutto trasparente per noi "utenti finali"
+        """)
+
+                ])
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        # Help!
+
+        Come ogni buon amico, Python offre alcune ricche funzionalità di _help_.
+        """
+    )
+    return
+
+
+@app.cell
+def _():
+    help(print)
+    return
+
+
+@app.cell
+def _():
+    help("if")
+    return
+
+
+@app.cell
+def _():
+    help("TRUTHVALUE")
+    return
+
+
+@app.cell
+def _():
+    help("symbols")
+    return
+
+
+if __name__ == "__main__":
+    app.run()
